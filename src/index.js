820c49a50880a16b603457704fee52e5a80c36f2
Petr <ivanov198405@gmail.com>
2024-11-04 21:35:37 +0300

    2 zadanie  bekend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
 
 const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    const query = parsedUrl.query;
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+    // Обработка параметра ?hello=<name>
+    if (query.hello !== undefined) {
+        if (query.hello) {
+            // Если передано имя
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end(`Hello, ${query.hello}.`);
+        } else {
+            // Если параметр `hello` указан, но имя не передано
+            response.writeHead(400, { 'Content-Type': 'text/plain' });
+            response.end('Enter a name');
+        }
+    } else if (query.users !== undefined) {
+        // Обработка параметра ?users
+        fs.readFile('src/data/users.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(err)
+                response.writeHead(500, { 'Content-Type': 'application/json' });
+                response.end(JSON.stringify({ error: 'Error reading users file' }));
+            } else {
+                response.writeHead(200, { 'Content-Type': 'application/json' });
+                response.end(data);
+            }
+        });
+    } else if (Object.keys(query).length === 0) {
+        // Обработка запроса без параметров
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end('Hello, World!');
+    } else {
+        // Обработка неизвестных параметров
+        response.writeHead(500);
+        response.end();
+    }
+});
 
+server.listen(3003, '127.0.0.1', () => {
+    console.log('Server is running at http://127.0.0.1:3003');
 });
